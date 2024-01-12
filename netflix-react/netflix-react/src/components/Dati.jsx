bc68d0c315215553c5cac19209a092cd2ea9e5a0
xhoana2322 <xhoana.beqiri23@gmail.com>
2024-01-12 17:38:02 +0100

    fetchino...ino ino

---
+++ netflix-react/netflix-react/src/components/Dati.jsx
@@ -0,0 +1,2 @@
+filmAPIkey = "971cc305"
+filmUrl = `http://www.omdbapi.com/?apikey=${filmAPIkey}&s=`
--- netflix-react/netflix-react/src/components/TrendingNow.jsx
+++ netflix-react/netflix-react/src/components/TrendingNow.jsx
@@ -1,24 +1,51 @@
 import React from 'react'
 import { Component } from 'react'
+import { Row, Col } from 'react-bootstrap'
+
+
 
 export default class TrendingNow extends Component {
 
+
   state = {
-    film: []
+    banana: [],
+    isLoading: false,
+    errorMsg: ""
   }
 
-  // componentDidMount() {
-  //   this.setState({
-  //     film: []
-  //   })
-  // }
+  getBananaFilm = async () => {
+    this.setState({ isLoading: true, errorMsg: "" });
+    await fetch("http://www.omdbapi.com/?apikey=971cc305&s=banana")
+        .then(response => {
+          if(response != ok) {
+            this.setState({ errorMsg: "errore nel fetch" });
+          } else {
+            console.log(res);
+            return res.json();
+          }
+        })
+
+
+        // .then(json => this.setState({
+        //           film: json }))
+        // .catch(err => console.error(err))
+  }
+
+
 
 
   render() {
     return (
     <>
       <div>
-        <h4 className='text-white mb-2'>Trending Now</h4> 
+        <h4 className='text-white mb-2'>Trending Now</h4>
+          <Row>
+          {this.state.film.map((a, index) => (
+            <Col key={index.imdbID}> 
+            {a.title}
+            <Col />
+          ))}
+          </Row> 
         
       </div>
     </>    
